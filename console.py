81c5474efd0a96101e0ff696333a1c176013c08f
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-08-31 13:48:36 +0000

    Update on console file

--- console.py
+++ console.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 """AirBnB command interpreter"""
 import cmd
+import re
+from shlex import split
+from models.base_model import BaseModel
+
+classes = [
+    "BaseModel"
+]
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
 class AirBnBShell(cmd.Cmd):
     """Command processor for our AirBnB Project."""
     prompt = '(hbnb) '
