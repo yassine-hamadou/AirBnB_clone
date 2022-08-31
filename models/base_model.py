081c6c6715b94a8de1064480785567788ce8004c
yassine <yassinehamadou1@gmail.com>
2022-08-31 02:05:39 +0000

    Writing __str__ function

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-import uuid import 
+from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
     def __init__(self):
-       self.id = str(uuid.uuid4())
+       self.id = str(uuid4())
        self.created_at = datetime.now()
        self.updated_at = datetime.now()
 
     def __str__(self):
-        string_representation = f"[{self.__class__}] ({self.id}) {self.__dict__}"
+        string_representation = f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
         return string_representation
+
+if __name__ == '__main__':
+    my_model = BaseModel()
+    print(my_model)
        
