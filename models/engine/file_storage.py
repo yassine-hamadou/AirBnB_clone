860f92139ba720e30eb60795874e3075d5cafe27
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-09-02 13:45:21 +0000

    Update on Engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,9 +5,11 @@ This contains the module definition for the file engine
 
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 classes = {
-    "BaseModel": BaseModel
+    "BaseModel": BaseModel,
+    "User": User
 }
 
 
