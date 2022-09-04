c3fdf26aba839e3342530f43833646f7754677f9
yassine <yassinehamadou1@gmail.com>
2022-09-04 15:12:01 +0000

    Created tesets/test_models

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains the BaseModel class that defines all common
+attributes/methods for other classes
+"""
 
 from uuid import uuid4
 from datetime import datetime
