c3a0cfda0e86c08b4f573c491d54bf73fdfa73d2
yassine <yassinehamadou1@gmail.com>
2022-09-04 23:49:51 +0000

    Update file_storage to comply to pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,16 +10,20 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+    
+    
     def all(self):
         """
         Returns self.__object which is a dictionary
         """
         return self.__objects
+    
     def new(self, obj):
         """
         sets in __objects the `obj` with key <obj class name>.id
         """
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        
     def save(self):
         """
         serializes `__objects` to the JSON file in `__file_path`
@@ -29,6 +33,7 @@ class FileStorage:
             dict_storage[k] = v.to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(dict_storage, f)
+            
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
@@ -40,3 +45,4 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
+        
