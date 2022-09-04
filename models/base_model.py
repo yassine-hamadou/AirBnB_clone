5ed443993fc590ca1f488a92384ed748a84276a2
yassine <yassinehamadou1@gmail.com>
2022-09-04 14:59:41 +0000

    created test/test_models

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-This module contains the BaseModel class that defines all common
-attributes/methods for other classes
-"""
 
 from uuid import uuid4
 from datetime import datetime
